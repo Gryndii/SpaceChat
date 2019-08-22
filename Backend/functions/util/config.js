90c9098a939c5b06512d8c2a666940b92bdec2e2
Gryndii <r.gryndii@gmail.com>
2019-08-22 19:47:43 +0300

    config removed

--- Backend/functions/util/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyC_3fetBJV-FpA_Y3cvG2scaVi-Iyao6cQ",
-    authDomain: "socialapp-f98e1.firebaseapp.com",
-    databaseURL: "https://socialapp-f98e1.firebaseio.com",
-    projectId: "socialapp-f98e1",
-    storageBucket: "socialapp-f98e1.appspot.com",
-    messagingSenderId: "1089332339541",
-    appId: "1:1089332339541:web:c4991cc3a46a9706"
-};
